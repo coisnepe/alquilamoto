8a922e2b82adf5c9ce36ad5161e3ecd9625fd3e0
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-08-24 21:01:01 +0200

    added devise for User model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,14 +2,3 @@ class Booking < ActiveRecord::Base
   belongs_to :bike
   belongs_to :user
 end
-
-
-# create_table "bookings", force: :cascade do |t|
-#     t.date     "date_begin"
-#     t.date     "date_end"
-#     t.integer  "total_price"
-#     t.integer  "bike_id"
-#     t.integer  "user_id"
-#     t.datetime "created_at",  null: false
-#     t.datetime "updated_at",  null: false
-#   end
