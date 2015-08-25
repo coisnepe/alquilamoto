e227ddaffb0450ca82c210f430b48a4ce64a4a0a
Rafael Perez <rapegoto@gmail.com>
2015-08-25 23:42:58 +0200

    add link to profil in navbar. Add user controller, route and show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :bikes
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
