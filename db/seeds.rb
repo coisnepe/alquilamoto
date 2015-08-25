7bcbd75dc460ce32ec45e8c4fbca295c01fa0174
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-08-25 12:09:50 +0200

    added a few bikes to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Bike.destroy_all
+
+bikes = [
+  {
+    type: "roadster",
+    description: "Kawasaki ZR7",
+    price_per_day: 100
+  },
+  {
+    type: "sportive",
+    description: "Honda CBR 600",
+    price_per_day: 150
+  },
+  {
+    type: "chopper",
+    description: "Harley-Davidson Roadster",
+    price_per_day: 150
+  },
+  {
+    type: "Dual Sport",
+    description: "Husqvarna Nuda",
+    price_per_day: 120,
+  },
+
+]
+
+bikes.each { |bike| Bike.create(bike) }
