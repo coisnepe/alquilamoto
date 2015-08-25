04ad49e5241edb4e29ba4bd7b0fda945cb0b1f5a
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-08-25 14:09:51 +0200

    added city to bikes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825105409) do
+ActiveRecord::Schema.define(version: 20150825120727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150825105409) do
     t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "city"
   end
 
   add_index "bikes", ["user_id"], name: "index_bikes_on_user_id", using: :btree
