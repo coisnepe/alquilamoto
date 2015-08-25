cfbc129e940c42107c1d0534e8b22e8b832f8380
Rafael Perez <rapegoto@gmail.com>
2015-08-25 12:32:42 +0200

    add associations to bike and booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ActiveRecord::Base
   belongs_to :bike
   belongs_to :user
-
+  belongs_to :customer, class_name: "User", foreign_key: :user_id
   validates :date_begin, presence: true
   validates :date_end, presence: true
   validate :total_price, presence: true
