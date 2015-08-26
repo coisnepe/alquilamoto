d9fb131dffbbe2eefb628e3ebf34c44b177e25b1
Rafael Perez <rapegoto@gmail.com>
2015-08-26 12:00:18 +0200

    added authorisation to update profile (in controller user), add thoses fields in edit view, add css to that view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+  def configure_permitted_parameters
+    # Comment RP: i didn'i added the email, so in theiry customer can't update it
+    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :first_name, :last_name, :phone_number, :date_birth, :date_driver_licence) }
+    devise_parameter_sanitizer.for(:account_update) << :first_name
+    devise_parameter_sanitizer.for(:account_update) << :last_name
+    devise_parameter_sanitizer.for(:account_update) << :phone_number
+    devise_parameter_sanitizer.for(:account_update) << :date_birth
+    devise_parameter_sanitizer.for(:account_update) << :date_driver_licence
+  end
+
 end
