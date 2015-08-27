a24d364ca4b139b95af1450973bcbddadd39616c
Rafael Perez <rapegoto@gmail.com>
2015-08-27 16:06:20 +0200

    rajout de 20.000 trucs avec Maxime: namespace, account controller, views. Delete some views, actions, etc...

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -7,17 +7,6 @@ class BikesController < ApplicationController
     @bike = Bike.find(params[:id])
   end
 
-  def new
-    @bike = Bike.new
-  end
-
-  def create
-    @bike = Bike.new(bike_params)
-    @bike.owner = current_user
-    @bike.save
-    redirect_to bike_path(@bike)
-  end
-
   private
 
   def bike_params
