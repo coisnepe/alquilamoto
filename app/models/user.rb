37144d0b8bcd7813ab26ee1fc2cc5951e9a14eae
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-08-28 16:05:06 +0200

    fixed email redirect

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  after_create :send_welcome_email
+  #after_create :send_welcome_email
 
   has_many :bookings
   has_many :bikes
